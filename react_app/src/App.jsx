964768752ab3f7c114c7af0874c4f1ff95c5efde
ryonakamura-st-mac <nakamura.ryo@seedtech.co.jp>
2023-05-11 08:55:45 +0800

    fixed react app malfunction

---
+++ react_app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import MapContainer from "./components/MapContainer";
+
+function App() {
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
