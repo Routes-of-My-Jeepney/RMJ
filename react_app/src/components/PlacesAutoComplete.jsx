3b7e6327794977f092c6c606cd9f677ac2583c44
Ryomarugon <murakami.ryoma@geechs.com>
2023-05-18 18:34:44 +0800

    出発地から目的地までの距離と所要時間の表示機能

--- react_app/src/components/PlacesAutoComplete.jsx
+++ react_app/src/components/PlacesAutoComplete.jsx
@@ -22,7 +22,6 @@ function PlacesAutoComplete() {
    const [placeOriginId, setPlaceOriginId] = useState(null);
    const [placeDestinationId, setPlaceDestinationId] = useState(null);
 
-
    const handleChange1 = (event) => {
       setInputValue1(event.target.value);
       console.log(inputValue1);
@@ -46,12 +45,11 @@ function PlacesAutoComplete() {
    var autocomplete_origin = new google.maps.places.Autocomplete(input_origin);
    autocomplete_origin.addListener("place_changed", function () {
       var place_origin = autocomplete_origin.getPlace();
-
       if (!place_origin.place_id) {
          console.log('error');
          return;
       }
-      console.log('success')
+      // console.log('success')
       handlePlaceOriginId(place_origin.place_id);
    })
 
@@ -65,7 +63,7 @@ function PlacesAutoComplete() {
          console.log('error');
       }
 
-      console.log('success');
+      // console.log('success');
       handlePlaceDestinationId(place_destination.place_id);
    })
 
@@ -73,11 +71,12 @@ function PlacesAutoComplete() {
       if (inputValue1 && inputValue2) {
          // const origin = originRef.current.value;
          // const destination = destRef.current.value;
-         
-         console.log(typeof placeOriginId);
-         console.log(typeof placeDestinationId);
+
+         // console.log(typeof placeOriginId);
+         // console.log(typeof placeDestinationId);
          var directionsService = new google.maps.DirectionsService();
          var directionsRenderer = new google.maps.DirectionsRenderer();
+         var distanceMatrixservice = new google.maps.DistanceMatrixService();
 
          var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 7,
@@ -88,44 +87,87 @@ function PlacesAutoComplete() {
             origin: { placeId: placeOriginId },
             destination: { placeId: placeDestinationId },
             travelMode: "DRIVING"
-         };
+         }
 
+         // ルート取得
+         // setLocation(request.origin, request.destination);
+         setLocation(request.origin.placeId, request.destination.placeId);
 
-         directionsService.route(request, function (result, status) {
+         function setLocation(originPlaceId, destinationPlaceId) {
+            // 所要時間取得
+            distanceMatrixservice.getDistanceMatrix({
+               origins: [{ placeId: originPlaceId }], // 出発地
+               destinations: [{ placeId: destinationPlaceId }], // 目的地
+               travelMode: request.travelMode, // 移動手段
+            }, timeRequired)
+
+            directionsService.route(request, function (result, status) {
+               if (status == "OK") {
+                  directionsRenderer.setDirections(result);
+               }
+            });
+         }
+
+         function timeRequired(response, status) {
             if (status == "OK") {
-               directionsRenderer.setDirections(result);
+               var origins = response.originAddresses;
+               var destinations = response.destinationAddresses;
+               for (var i = 0; i < origins.length; i++) {
+                  var results = response.rows[i].elements;
+                  for (var j = 0; j < results.length; j++) {
+                     var element = results[j];
+                     if (element.status === "OK") {
+                        var distance = element.distance.text;
+                        var duration = element.duration.text;
+                        var from = origins[i];
+                        var to = destinations[j];
+                        // console.log("距離: " + distance);
+                        // console.log("所要時間: " + duration);
+                     } else {
+                        // console.log("距離と所要時間の取得に失敗しました。");
+                     }
+                  }
+               }
+               var routeDistance = document.getElementById("route-distance");
+               routeDistance.innerHTML = "距離：" + distance
+
+               var routeTime = document.getElementById("route-time");
+               routeTime.innerHTML = "およそ" + duration + "で着きます"
+            } else {
+               // console.log("距離と所要時間の取得に失敗しました。ステータス: " + status);
             }
-         });
+
+         }
       };
-   };
-
-
-
-      useEffect(() => {
-         autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-            originRef.current,
-            options
-         );
-         autoCompleteRef2.current = new window.google.maps.places.Autocomplete(
-            destRef.current,
-            options
-         );
-         // autoCompleteRef.current.addListener("place_changed", async function () {
-         //  const place = await autoCompleteRef.current.getPlace();
-         //  console.log({ place });
-         // });
-      }, []);
-
-      return (
-         <div>
-            <label>出発地 :</label>
-            <input id="origin" ref={originRef} onChange={handleChange1} />
-            <br />
-            <label>目的地 :</label>
-            <input id="destination" ref={destRef} onChange={handleChange2} />
-            <input type='submit' value="ルートを開始" onClick={startRoute} />
-         </div>
-      );
    }
 
-   export default PlacesAutoComplete;
+   useEffect(() => {
+      autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+         originRef.current,
+         options
+      );
+      autoCompleteRef2.current = new window.google.maps.places.Autocomplete(
+         destRef.current,
+         options
+      );
+      // autoCompleteRef.current.addListener("place_changed", async function () {
+      //  const place = await autoCompleteRef.current.getPlace();
+      //  console.log({ place });
+      // });
+   }, []);
+
+   return (
+      <div>
+         <label>出発地 :</label>
+         <input id="origin" ref={originRef} onChange={handleChange1} />
+         <br />
+         <label>目的地 :</label>
+         <input id="destination" ref={destRef} onChange={handleChange2} />
+         <input type='submit' value="ルートを開始" onClick={startRoute} />
+         <div><p id="route-distance"></p></div>
+         <div><p id="route-time"></p></div>
+      </div>
+   );
+}
+
+export default PlacesAutoComplete;
