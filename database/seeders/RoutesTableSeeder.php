4abf7c6199c8a34fc68f87ba1be057f4c0da4d38
ryonakamura-st-mac <nakamura.ryo@seedtech.co.jp>
2023-05-18 22:07:59 +0800

    Added routes model and test data

---
+++ database/seeders/RoutesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Route;
+
+class RoutesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Route::factory()
+            ->count(50)
+            ->create();
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Route;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::factory()
+            ->count(50)
+            ->has(Route::factory()->count(3))
+            ->create();
+    }
+}
