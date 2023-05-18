e67e3ee1225afceb4c04bb728049dd7a80d9d0d2
ryo-nakamura-st <125847132+ryo-nakamura-st@users.noreply.github.com>
2023-05-18 11:23:27 +0800

    Revert "経路ルート表示機能の作成"

--- react_app/src/components/MapContainer.jsx
+++ react_app/src/components/MapContainer.jsx
@@ -1,10 +1,8 @@
-
-import React, { useState, useEffect, useRef } from 'react';
-import { GoogleMap, LoadScript, MarkerF,DirectionsRenderer } from '@react-google-maps/api';
-import  PlacesAutoComplete  from './PlacesAutoComplete';
+import React , {useState, useEffect, useRef } from 'react';
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import PlacesAutoComplete from './PlacesAutoComplete';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-
 
 const containerStyle = {
   width: '100%',
@@ -15,10 +13,9 @@ function MapContainer() {
   const mapRef = useRef();
   const [center, setCenter] = useState();
   const [MarkerPosition, setMarkerPosition] = React.useState();
-  const [directions, setDirections] = useState(null);
 
   React.useEffect(() => {
-    if (navigator.geolocation) {
+    if (navigator.geolocation){
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const coords = {
@@ -36,41 +33,29 @@ function MapContainer() {
     } else {
       console.log('Geolocation is not supported by this browser.');
     }
-  }, []);
-
-
-  // useEffect(() => {
-  //   console.log('MarkerPosition updated:', MarkerPosition);
-  // }, [MarkerPosition])
+      }, []);
+  
+  useEffect(() => {
+    console.log('MarkerPosition updated:', MarkerPosition);
+  }, [MarkerPosition])
 
   const onLoad = React.useCallback(map => (mapRef.current = map), []);
-  
+
+
   return (
     <>
-     <Container>
+    <Container>
       <Box>
         <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY} libraries={['places']}>
-          <PlacesAutoComplete />
-          <GoogleMap
-            id='map'
-            ref={mapRef}
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}>
-            {directions && (
-              <DirectionsRenderer
-                directions={directions}
-                options={{
-                  polylineOptions: {
-                    strokeColor: 'blue',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 4,
-                  },
-                }}
-              />
-            )}
-            <MarkerF position={MarkerPosition} />
+          <GoogleMap 
+          ref={mapRef}
+          mapContainerStyle={containerStyle} 
+          center={center} 
+          zoom={10}
+          onLoad={onLoad}
+          >
+          <PlacesAutoComplete/>
+            <MarkerF position={MarkerPosition}/>
           </GoogleMap>
         </LoadScript>
       </Box>
