56b6e95159293618174a9c610a64823996dc49a4
Ryomarugon <murakami.ryoma@geechs.com>
2023-06-06 16:06:04 +0800

    validation（サインアップ・アカウント情報変更部分）

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
@@ -32,11 +32,11 @@ class UserController extends Controller
     }
 
     public function likedJeepneys(Request $request)
-{
-    $jeepneys = $request->user()->likedJeepneys();
+    {
+        $jeepneys = $request->user()->likedJeepneys();
 
-    return response()->json($jeepneys, 200);
-}
+        return response()->json($jeepneys, 200);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -82,53 +82,53 @@ class UserController extends Controller
      */
 
     public function update(Request $request)
-{
-    try{
-    $user = Auth::user();
-    Log::info('Request Data:', $request->all()); 
-    // Validate the request...
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|email|unique:users,email,'.$user->id,
-        'profile_img' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    ]);
+    {
+        try {
+            $user = Auth::user();
+            Log::info('Request Data:', $request->all());
+            // Validate the request...
+            $request->validate([
+                'name' => 'required|string|max:55',
+                'email' => 'required|email|unique:users,email,' . $user->id,
+                'profile_img' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
 
-    // Handle profile image upload...
-    if ($request->hasFile('profile_img')) {
-        $image = $request->file('profile_img');
-        $filename = time().'.'.$image->getClientOriginalExtension();
-        $path = $image->storeAs('public/images', $filename);
-        $user->profile_img = $filename;
+            // Handle profile image upload...
+            if ($request->hasFile('profile_img')) {
+                $image = $request->file('profile_img');
+                $filename = time() . '.' . $image->getClientOriginalExtension();
+                $path = $image->storeAs('public/images', $filename);
+                $user->profile_img = $filename;
+            }
+
+            // Update name...
+            $user->name = $request->name;
+
+            // Update email...
+            $user->email = $request->email;
+
+            // Save the changes...
+            $user->save();
+
+            return response()->json([
+                'message' => 'Successfully updated user!',
+                'user' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            Log::error('Error:', ['message' => $e->getMessage()]);
+            return response()->json([
+                'message' => 'Error updating user!',
+                'error' => $e->getMessage(),
+            ], 409);
+        }
     }
-
-    // Update name...
-    $user->name = $request->name;
-
-    // Update email...
-    $user->email = $request->email;
-
-    // Save the changes...
-    $user->save();
-
-    return response()->json([
-        'message' => 'Successfully updated user!',
-        'user' => $user,
-    ], 200);
-    }catch(\Exception $e){
-        Log::error('Error:',['message' => $e->getMessage()]);
-        return response()->json([
-            'message' => 'Error updating user!',
-            'error' => $e->getMessage(),
-        ], 409);
-    }
-}
     // public function update(Request $request, $id)
     // {
     //     // ログイン中のユーザの情報を取得し、$userに代入
     //     $user = Auth::user();
 
     //     // リクエストデータを取得し、$updateUserに代入
-        // $updateUser = $request->all();
+    // $updateUser = $request->all();
 // 
     //     // プロフィール画像の変更があった場合
     //     if ($request->hasFile('profile-img')) {
@@ -162,8 +162,48 @@ class UserController extends Controller
     public function delete(User $user)
     {
         $user->delete();
-        return response()->json(null,204);
+        return response()->json(null, 204);
     }
+
+    public function resetPassword(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            Log::info('Request Data:', $request->all());
+            // Validate the request...
+            $request->validate([
+                'currentPassword' => 'required',
+                'newPassword' => [
+                    'required',
+                    Password::min(8)
+                        ->letters()
+                        ->symbols(),
+                ]
+            ]);
+
+            if (!Hash::check($request->currentPassword, $user->password)) {
+                return response()->json([
+                    'message' => 'Current password is incorrect.',
+                ], 400);
+            }
+
+            // Update name...
+            $user->password =  Hash::make($request->newPassword);
+            $user->save();
+
+            return response()->json([
+                'message' => 'Successfully updated password!',
+                'user' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            Log::error('Error:', ['message' => $e->getMessage()]);
+            return response()->json([
+                'message' => 'Error updating password!',
+                'error' => $e->getMessage(),
+            ], 409);
+        }
+    }
+
     public function deleteProfileImage()
     {
         $user = Auth::user();
@@ -171,7 +211,7 @@ class UserController extends Controller
             Storage::delete('public/profiles' . $user->profile_img); // ファイルをストレージから削除
             $user->profile_img = null; // プロフィール画像のパスをnullに更新
             $user->save(); // ユーザーモデルを保存
-            return redirect()->route('users.show',compact('user'))->with('success', 'プロフィール画像を削除しました。');
+            return redirect()->route('users.show', compact('user'))->with('success', 'プロフィール画像を削除しました。');
         } else {
             return redirect()->back()->with('error', 'プロフィール画像が存在しません。');
         }
